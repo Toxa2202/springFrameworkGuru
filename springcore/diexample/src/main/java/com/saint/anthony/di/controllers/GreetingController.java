477c8e727c93bc8fc954ec93a70d4d0e3bae8784
Toxa2202 <svyatoj@gmail.com>
2019-10-08 14:20:24 +0300

    Spring XML config without annotations.

--- springcore/diexample/src/main/java/com/saint/anthony/di/controllers/GreetingController.java
+++ springcore/diexample/src/main/java/com/saint/anthony/di/controllers/GreetingController.java
@@ -6,26 +6,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
-@Controller
 public class GreetingController {
     private HelloWorldService helloWorldService;
     private HelloWorldService helloWorldServiceGerman;
     private HelloWorldService helloWorldServiceFrench;
 
-    @Autowired
     public void setHelloWorldService(HelloWorldService helloWorldService) {
         this.helloWorldService = helloWorldService;
     }
 
-    @Autowired
-    @Qualifier("helloWorldServiceGerman")
     public void setHelloWorldServiceGerman(HelloWorldService helloWorldServiceGerman) {
         this.helloWorldServiceGerman = helloWorldServiceGerman;
     }
 
-    @Autowired
-    @Qualifier("french")
-    public void setGetHelloWorldServiceFrench(HelloWorldService helloWorldServiceFrench) {
+    public void setHelloWorldServiceFrench(HelloWorldService helloWorldServiceFrench) {
         this.helloWorldServiceFrench = helloWorldServiceFrench;
     }
 
