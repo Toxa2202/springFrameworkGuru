6698dcc0b23bedd8f279b21798d2d5935e34947e
Toxa2202 <svyatoj@gmail.com>
2019-10-08 19:45:26 +0300

    Continue SpringMVC app. Create list of Products and show it on the screen as a table products.html with bootstrap. TimeCode 2:25:20.

---
+++ springcore/spring-mvc/src/main/java/com/saint/anthony/services/ProductService.java
@@ -0,0 +1,9 @@
+package com.saint.anthony.services;
+
+import com.saint.anthony.domain.Product;
+
+import java.util.List;
+
+public interface ProductService {
+    List<Product> listAllProducts();
+}
