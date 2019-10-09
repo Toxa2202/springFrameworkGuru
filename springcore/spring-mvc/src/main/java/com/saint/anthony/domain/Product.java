5c5a27dda23471b8c6c1e50f40e53926423e1504
Toxa2202 <svyatoj@gmail.com>
2019-10-09 19:50:01 +0300

    springframeworkGuru project. Creating new implementation using JPA. TimeCode 4:22:00

--- springcore/spring-mvc/src/main/java/com/saint/anthony/domain/Product.java
+++ springcore/spring-mvc/src/main/java/com/saint/anthony/domain/Product.java
@@ -1,9 +1,6 @@
 package com.saint.anthony.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.math.BigDecimal;
 
 @Entity
@@ -12,12 +9,23 @@ public class Product implements DomainObject {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
+    @Version
+    private Integer version;
+
     private String description;
     private BigDecimal price;
     private String imageUrl;
 
     public Integer getId() {
         return id;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
     }
 
     public void setId(Integer id) {
--- springcore/spring-mvc/src/main/java/com/saint/anthony/services/ProductServiceImpl.java
+++ springcore/spring-mvc/src/main/java/com/saint/anthony/services/ProductServiceImpl.java
@@ -2,11 +2,13 @@ package com.saint.anthony.services;
 
 import com.saint.anthony.domain.DomainObject;
 import com.saint.anthony.domain.Product;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import java.math.BigDecimal;
 import java.util.*;
 
 @Service
+@Profile("map")
 public class ProductServiceImpl extends AbstractMapService implements ProductService {
 
     @Override
