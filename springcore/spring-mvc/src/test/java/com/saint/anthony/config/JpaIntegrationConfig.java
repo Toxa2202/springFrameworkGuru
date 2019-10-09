abea2e459f8de9020ed287c5ffe1cef436c74221
Toxa2202 <svyatoj@gmail.com>
2019-10-09 20:32:36 +0300

    springframeworkGuru project. Add Integration test with JPA configuration file. TimeCode 4:45:00

---
+++ springcore/spring-mvc/src/test/java/com/saint/anthony/config/JpaIntegrationConfig.java
@@ -0,0 +1,11 @@
+package com.saint.anthony.config;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan("com.saint.anthony")
+public class JpaIntegrationConfig {
+}
